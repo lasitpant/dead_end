5b7fa8cabfe8d1d40d2bbb8684a1e6f790761ae6
lasitpant <lasitpant19@gmail.com>
2016-11-29 22:42:08 +0000

    Update index.php

--- index.php
+++ index.php
@@ -90,8 +90,8 @@ require_once('bdd.php');
 
         <div class="row">
             <div class="col-lg-12 text-center">
-                <h1>FullCalendar BS3 PHP MySQL</h1>
-                <p class="lead">Complete with pre-defined file paths that you won't have to change!</p>
+                <h1>Event Calendar</h1>
+                <p class="lead"></p>
                 <div id="calendar" class="col-centered">
                 </div>
             </div>
