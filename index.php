52b3bd67a200b98d177d9bbdf98e9cce07e18bdf
lasitpant <lasitpant19@gmail.com>
2016-11-29 22:59:44 +0000

    Update index.php

--- index.php
+++ index.php
@@ -5,6 +5,22 @@ require_once('bdd.php');
 //require_once('create.php');
 $conn = connection();
 
+$sql = "CREATE TABLE events(
+			id int(11) NOT NULL AUTO_INCREMENT,
+                        title varchar(255) NOT NULL,
+                        color varchar(7) DEFAULT NULL,
+                        start datetime NOT NULL,
+                        end datetime DEFAULT NULL,
+                        PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=15 ;"
+	
+try{
+	$conn->query($sql);
+}
+catch(Exception $e){
+	print_r($e);
+}
+echo "<h3>Table created.</h3>";
 
 //$sql = "SELECT id, title, start, end, color FROM events ";
 
