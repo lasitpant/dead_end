913aa8f38d4df692607e75dc5d784356c5beedc5
lasitpant <lasitpant19@gmail.com>
2016-11-29 18:12:24 +0000

    Update bdd.php

--- bdd.php
+++ bdd.php
@@ -1,7 +1,7 @@
 <?php
 try
 {
-	$bdd = new PDO('mysql:host=localhost;dbname=calendar;charset=utf8', 'root', '');
+	$bdd = new PDO('mysql:host=tcp:newcalendar.database.windows.net;dbname=newDb;charset=utf8', 'rituraj', 'lasit@1234');
 }
 catch(Exception $e)
 {
