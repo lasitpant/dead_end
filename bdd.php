cce33f99292980b32628c02ab8d13c3040bc265d
lasitpant <lasitpant19@gmail.com>
2016-11-29 22:20:18 +0000

    Update bdd.php

--- bdd.php
+++ bdd.php
@@ -1,9 +1,15 @@
 <?php
+
+        $host = "eu-cdbr-azure-west-a.cloudapp.net";
+	$user = "b1929b8315b135";
+	$pwd = "c752bb86";
+	$db = "newcalendardb";
+
 try
 {
 	
-	$bdd = new PDO("sqlsrv:server = tcp:newcalendar.database.windows.net,1433; Database = newDb", "lasit", "Pant@1234");
-        $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	$conn = new PDO( "mysql:host=$host;dbname=$db", $user, $pwd);
+        $conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
 }
 catch(Exception $e)
 {
