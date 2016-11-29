7cfb50acd90e2af04ac9376ed5af17fa7b4ee8d4
lasitpant <lasitpant19@gmail.com>
2016-11-29 22:53:39 +0000

    Update bdd.php

--- bdd.php
+++ bdd.php
@@ -1,5 +1,7 @@
 <?php
 
+function connection()
+{	
         $host = "eu-cdbr-azure-west-a.cloudapp.net";
 	$user = "b1929b8315b135";
 	$pwd = "c752bb86";
@@ -15,21 +17,5 @@ catch(Exception $e)
 {
         die('Error : '.$e->getMessage());
 }
-CREATE TABLE `events` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `title` varchar(255) NOT NULL,
-  `color` varchar(7) DEFAULT NULL,
-  `start` datetime NOT NULL,
-  `end` datetime DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=15 ;
-
-//try{
-//	$conn->query($sql);
-//}
-//catch(Exception $e){
-//	print_r($e);
-//}
-//echo "<h3>Table created.</h3>";
-
+}
 ?>
