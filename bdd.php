46170d8296619655f0ffd70c44d81cc4aec3addc
lasitpant <lasitpant19@gmail.com>
2016-11-30 23:43:37 +0000

    Update bdd.php

--- bdd.php
+++ bdd.php
@@ -27,7 +27,7 @@ if (!$link) {
 
 echo "Success: A proper connection to MySQL was made! The my_db database is great." . PHP_EOL;
 echo "Host information: " . mysqli_get_host_info($link) . PHP_EOL;
-$sql = "SELECT * from neweve.events";
+$sql = 'SELECT * from neweve.events';
 echo $sql;
 
 ?>
