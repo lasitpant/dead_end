ab8ba2f93b28f3f3aa96da834f785cdfba025f2a
lasitpant <lasitpant19@gmail.com>
2016-11-30 18:35:03 +0000

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,6 @@
 <?php
 require_once('bdd.php');
-$sql = "SELECT id, title, start, end, color FROM events ";
-echo $sql;
+$sql = SELECT id, title, start, end, color FROM events;
 //$req = $bdd->prepare($sql);
 //$req->execute();
 //$events = $req->fetchAll();
