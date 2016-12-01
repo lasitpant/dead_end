f4b5bb1262c67e2300e0285a68492bedab453b8e
lasitpant <lasitpant19@gmail.com>
2016-12-01 03:08:26 +0000

    Update bdd.php

--- bdd.php
+++ bdd.php
@@ -18,14 +18,14 @@ foreach ($_SERVER as $key => $value) {
 
 $link = mysqli_connect($connectstr_dbhost, $connectstr_dbusername, $connectstr_dbpassword,$connectstr_dbname);
 
-if (!$link) {
+/*if (!$link) {
     echo "Error: Unable to connect to MySQL." . PHP_EOL;
     echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
     echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
     exit;
-}
+}*/
 
 //echo "Success: A proper connection to MySQL was made! The my_db database is great." . PHP_EOL;
 //echo "Host information: " . mysqli_get_host_info($link) . PHP_EOL;
 
-?>
+
